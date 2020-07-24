6ecfd67dc72cbfbf05868124c3675230b226d1bc
gracenho829 <49267393+gracenho829@users.noreply.github.com>
2020-07-24 12:31:28 +0900

    firstProject
    firstProject

---
+++ ajitexCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ajite
+//
+//  Created by Chanwoong Ahn on 2020/07/23.
+//  Copyright © 2020 keilvher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
