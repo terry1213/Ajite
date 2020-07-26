32b2b48211479b4a0e93bc1fa264b169f88b0507
gracenho829 <49267393+gracenho829@users.noreply.github.com>
2020-07-26 20:19:09 +0900

    Added Constraints

--- ajite/ajite/DesignableView.swift
+++ ajite/ajite/DesignableView.swift
@@ -15,16 +15,19 @@ class DesignableView : UIView {
             layer.shadowColor = shadowColor.cgColor
         }
     }
-    @IBInspectable var shadowOpacity : CGFloat = 0{
-        didSet{
-            layer.shadowOpacity = Float(shadowOpacity)
-        }
-    }
+    
     @IBInspectable var shadowRadius : CGFloat = 0{
         didSet{
             layer.shadowRadius = shadowRadius
         }
     }
+    
+    @IBInspectable var shadowOpacity : CGFloat = 0{
+        didSet{
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
     @IBInspectable var shadowOffsetY : CGFloat = 0{
         didSet {
             layer.shadowOffset.height = shadowOffsetY
