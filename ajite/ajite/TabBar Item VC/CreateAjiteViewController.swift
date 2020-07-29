ba0e17165dcdc80c6afc5f272ccfb7838676f853
gracenho829 <49267393+gracenho829@users.noreply.github.com>
2020-07-29 19:09:01 +0900

    TabBar/ NC
    Tab Bar + Added NC

---
+++ ajite/ajite/TabBar Item VC/CreateAjiteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateAjiteViewController.swift
+//  ajite
+//
+//  Created by 노은솔 on 2020/07/29.
+//  Copyright © 2020 ajite. All rights reserved.
+//
+
+import UIKit
+
+class CreateAjiteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
