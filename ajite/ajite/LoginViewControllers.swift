0bbc9812200e80866d59a0193a168b3f6a7634d7
keiluv <21800510@handong.edu>
2020-07-30 19:26:23 +0900

    firebase login
    firebase login, please pod install and pod update when you pull this file

--- ajite/ajite/LoginViewControllers.swift
+++ ajite/ajite/LoginViewControllers.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if (error == nil){
+            
+        }else{
+            
+        }
+        
+    }
+    
+    /*
+    func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!, withError error: NSError){
+        if (error == nil){
+            
+        }
+    }
+    */
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance().delegate = self//uiDelegate to delegate
+        GIDSignIn.sharedInstance()?.signIn()
         // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
 
 }
