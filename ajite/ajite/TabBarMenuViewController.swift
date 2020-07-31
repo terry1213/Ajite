c97fdfbdee3b34ede332c04f8498070ee325ea59
Yeonwoo Lim <1213terry@naver.com>
2020-07-31 19:11:41 +0900

    TabBarMenuViewController 추가
    TabBar를 위한 controller class 추가

---
+++ ajite/ajite/TabBarMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarMenuViewController.swift
+//  ajite
+//
+//  Created by 임연우 on 2020/07/31.
+//  Copyright © 2020 ajite. All rights reserved.
+//
+
+import UIKit
+
+class TabBarMenuViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 2
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
