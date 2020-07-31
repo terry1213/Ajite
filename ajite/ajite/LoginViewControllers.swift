761715f3f2256ad036bcc0c78b9d689ba7b331be
Yeonwoo Lim <1213terry@naver.com>
2020-07-31 19:13:53 +0900

    sign 함수 구현
    구글 로그인을 위한 sign 함수 구현

--- ajite/ajite/LoginViewControllers.swift
+++ ajite/ajite/LoginViewControllers.swift
@@ -13,28 +13,52 @@ import GoogleSignIn
 class LoginViewController: UIViewController, GIDSignInDelegate {
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if (error == nil){
-            let webPage = self.storyboard?.instantiateViewController(withIdentifier: "test")
-            self.present(webPage!, animated: true, completion: nil)
+            guard let authentification = user.authentication else {return}
+                    let credential = GoogleAuthProvider.credential(withIDToken: authentification.idToken, accessToken: authentification.accessToken)
+                    
+                    Auth.auth().signIn(with: credential) { (authResult, error) in
+                      if let error = error {
+                        let authError = error as NSError
+                        if (authError.code == AuthErrorCode.secondFactorRequired.rawValue) {
+                          // The user is a multi-factor user. Second factor challenge is required.
+                          let resolver = authError.userInfo[AuthErrorUserInfoMultiFactorResolverKey] as! MultiFactorResolver
+                          var displayNameString = ""
+                          for tmpFactorInfo in (resolver.hints) {
+                            displayNameString += tmpFactorInfo.displayName ?? ""
+                            displayNameString += " "
+                          }
+                        } else {
+                          //self.showMessagePrompt(error.localizedDescription)
+                          return
+                        }
+                        // ...
+                        return
+                      }
+                      // User is signed in
+                      // ...
+                    }
+            let vcName = self.storyboard?.instantiateViewController(withIdentifier: "TabBarController")
+            vcName?.modalTransitionStyle = .coverVertical
+            self.present(vcName!, animated: true, completion: nil)
         }else{
             
         }
         
     }
     
-    /*
+    
     func signIn(signIn: GIDSignIn!, didSignInForUser user: GIDGoogleUser!, withError error: NSError){
         if (error == nil){
-            
         }
     }
-    */
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         GIDSignIn.sharedInstance()?.presentingViewController = self
-        //GIDSignIn.sharedInstance().delegate = self//uiDelegate to delegate
-        GIDSignIn.sharedInstance().signIn()
+        GIDSignIn.sharedInstance().delegate = self//uiDelegate to delegate
+        //GIDSignIn.sharedInstance().signIn()
         // Do any additional setup after loading the view.
     }
     
