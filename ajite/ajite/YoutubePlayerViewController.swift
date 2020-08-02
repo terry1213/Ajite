bd624dc6d917e89625c13a5a9e731b49716870d3
Yeonwoo Lim <1213terry@naver.com>
2020-08-02 17:43:46 +0900

    YoutubePlayerViewController 생성
    유튜브 영상 제생을 위한 YoutubePlayerViewController 생성

---
+++ ajite/ajite/YoutubePlayerViewController.swift
@@ -0,0 +1,51 @@
+//
+//  YoutubePlayerViewController.swift
+//  ajite
+//
+//  Created by 임연우 on 2020/08/01.
+//  Copyright © 2020 ajite. All rights reserved.
+//
+
+import UIKit
+import youtube_ios_player_helper
+
+class YoutubePlayerViewController: UIViewController, YTPlayerViewDelegate{
+    
+    var youtubePlayer: YTPlayerView!
+    let videoId: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func loadView() {
+        youtubePlayer = YTPlayerView()
+        self.youtubePlayer.delegate = self;
+        self.youtubePlayer.load(withVideoId: "xhyglLj3pQI", playerVars: [
+            "controls": 1,  //0: 영상 컨트롤 바 사라짐, 1: 컨트롤 바 생성
+            "playsinline": 1,   //0: 전체 화면 재생, 1: playerView 화면 내애서 재생
+        ])
+        view = youtubePlayer
+    }
+    
+    func playerViewDidBecomeReady(_ playerView: YTPlayerView) {
+        youtubePlayer.playVideo()
+    }
+    
+    func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
+        if state == YTPlayerState.ended {
+            print("finished")
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
+}
