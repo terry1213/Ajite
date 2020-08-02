f47e8af2997af851c9991e1317fc465aa0facfa1
Yeonwoo Lim <1213terry@naver.com>
2020-08-02 23:22:14 +0900

    영상 연속 재생 테스트
    String array를 통한 영상 연속 재생 테스트

--- ajite/ajite/YoutubePlayerViewController.swift
+++ ajite/ajite/YoutubePlayerViewController.swift
@@ -12,7 +12,8 @@ import youtube_ios_player_helper
 class YoutubePlayerViewController: UIViewController, YTPlayerViewDelegate{
     
     var youtubePlayer: YTPlayerView!
-    let videoId: String = ""
+    let videoId: [String] = ["xhyglLj3pQI", "l5i8YoFBvO4"]
+    var index: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +22,7 @@ class YoutubePlayerViewController: UIViewController, YTPlayerViewDelegate{
     override func loadView() {
         youtubePlayer = YTPlayerView()
         self.youtubePlayer.delegate = self;
-        self.youtubePlayer.load(withVideoId: "xhyglLj3pQI", playerVars: [
+        self.youtubePlayer.load(withVideoId: videoId[index], playerVars: [
             "controls": 1,  //0: 영상 컨트롤 바 사라짐, 1: 컨트롤 바 생성
             "playsinline": 1,   //0: 전체 화면 재생, 1: playerView 화면 내애서 재생
         ])
@@ -35,6 +36,13 @@ class YoutubePlayerViewController: UIViewController, YTPlayerViewDelegate{
     func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
         if state == YTPlayerState.ended {
             print("finished")
+            index += 1
+            if index < videoId.count{
+                self.youtubePlayer.load(withVideoId: videoId[index], playerVars: [
+                    "controls": 1,  //0: 영상 컨트롤 바 사라짐, 1: 컨트롤 바 생성
+                    "playsinline": 1,   //0: 전체 화면 재생, 1: playerView 화면 내애서 재생
+                ])
+            }
         }
     }
     /*
