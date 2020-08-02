b83d2029412384414c9f7dac71750bb28456c075
gracenho829 <49267393+gracenho829@users.noreply.github.com>
2020-08-02 17:39:59 +0900

    Circle Image

--- ajite/ajite/TabBar Item VC/InsidePlaylistViewController.swift
+++ ajite/ajite/TabBar Item VC/InsidePlaylistViewController.swift
@@ -1,14 +1,14 @@
 //
-//  HomeViewController.swift
+//  InsidePlaylistViewController.swift
 //  ajite
 //
-//  Created by 노은솔 on 2020/07/29.
+//  Created by 노은솔 on 2020/08/02.
 //  Copyright © 2020 ajite. All rights reserved.
 //
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class InsidePlaylistViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
