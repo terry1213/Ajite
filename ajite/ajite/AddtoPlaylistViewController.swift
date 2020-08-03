ba525aff73cb23b91503a0d31dcc5bbd7e2ed966
gracenho829 <49267393+gracenho829@users.noreply.github.com>
2020-08-03 19:47:30 +0900

    UI 수정했음
    UI 수정했음 (아지트)

---
+++ ajite/ajite/AddtoPlaylistViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddtoPlaylistViewController.swift
+//  ajite
+//
+//  Created by 노은솔 on 2020/08/03.
+//  Copyright © 2020 ajite. All rights reserved.
+//
+
+import UIKit
+
+class AddtoPlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    //플레이리스트 생성하고 디스미스하기
+    @IBAction func create(_ sender: Any) {
+        
+        dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
