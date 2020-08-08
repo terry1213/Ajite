f19a52515e84c43ab336b5b0bae767d283a85df4
keiluv <21800510@handong.edu>
2020-08-09 04:02:33 +0900

    request message send
    have to edit...

---
+++ ajite/ajite/RequestTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  RequestTableViewCell.swift
+//  ajite
+//
+//  Created by Chanwoong Ahn on 2020/08/09.
+//  Copyright © 2020 ajite. All rights reserved.
+//
+
+import UIKit
+protocol RequestTableViewUser {
+    func AcceptClickCell(index: Int)
+    func DeclineClickCell(index: Int)
+}
+
+class RequestTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameBox: UILabel!
+    var cellDelegate: RequestTableViewUser?
+    var index: IndexPath?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    @IBAction func accept(_ sender: Any) {
+        cellDelegate?.AcceptClickCell(index: (index?.row)!)
+    }
+    @IBAction func decline(_ sender: Any) {
+        cellDelegate?.DeclineClickCell(index: (index?.row)!)
+    }
+    
+}
