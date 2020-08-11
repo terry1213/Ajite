a1a04017cd9d8ca39760fe94ab8face4f8f1b360
Yeonwoo Lim <1213terry@naver.com>
2020-08-11 21:13:59 +0900

    ajite room view와 youtube player view 연결
    ajite room view와 youtube player view 연결

--- ajite/ajite/Ajite/AjiteRoomViewController.swift
+++ ajite/ajite/Ajite/AjiteRoomViewController.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import youtube_ios_player_helper
 
 //:::::::::::::아지트 방 안을 보여주는 뷰 컨트롤러 :::::::::::
 class AjiteRoomViewController: UIViewController {
     
     
    //아웃렛과 변수들
+    let youtubePlayerViewController = YoutubePlayerViewController()
     var currentAjite = Ajite()
+    var nextSourceIndex : Int = -1
     @IBOutlet weak var ajiteName: UILabel!
     @IBOutlet weak var sharedSongsView: UIView!
     @IBOutlet weak var background: UIImageView!
@@ -36,6 +39,7 @@ class AjiteRoomViewController: UIViewController {
         var random = arc4random_uniform(2)
         let randomBackground = "scroll\(random)"
         background.image = UIImage(named:randomBackground)
+        youtubePlayerViewController.youtubeVideos = currentAjite.sharedSongs
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -49,7 +53,7 @@ class AjiteRoomViewController: UIViewController {
                 if let err = err {
                     print("Error getting documents: \(err)")
                 } else {
-                    self.currentAjite.sharedSongs.removeAll()
+                    self.currentAjite.sharedSongs.songs.removeAll()
                     var temSong : Song
                     var count = 0
                     for document in querySnapshot!.documents {
@@ -60,7 +64,7 @@ class AjiteRoomViewController: UIViewController {
                         temSong.thumbnailImageUrl = data["thumbnailImageUrl"] as! String
                         temSong.videoID = data["videoID"] as! String
                         temSong.songID = document.documentID
-                        self.currentAjite.sharedSongs.append(temSong)
+                        self.currentAjite.sharedSongs.songs.append(temSong)
                         count += 1
                         if count == querySnapshot!.documents.count {
                             self.songCollection.reloadData()
@@ -68,6 +72,23 @@ class AjiteRoomViewController: UIViewController {
                     }
                 }
             }
+    }
+    
+    func addChildVC() {
+        addChild(youtubePlayerViewController)
+        UIView.transition(with: self.view, duration: 0.8, options: [.transitionCrossDissolve], animations: {
+            self.view.addSubview(self.youtubePlayerViewController.view)
+        }, completion: nil)
+        youtubePlayerViewController.didMove(toParent: self)
+        setYoutubePlayerVCConstraints()
+    }
+    
+    func setYoutubePlayerVCConstraints() {
+        youtubePlayerViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        youtubePlayerViewController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 90).isActive = true
+        youtubePlayerViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        youtubePlayerViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        youtubePlayerViewController.view.heightAnchor.constraint(equalToConstant: 180).isActive = true
     }
     
     //아지트 오른쪽 상단에 있는 member 버튼을 누르면 이 함수가 불러짐
@@ -94,7 +115,7 @@ class AjiteRoomViewController: UIViewController {
 
 extension AjiteRoomViewController : UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return currentAjite.sharedSongs.count
+        return currentAjite.sharedSongs.songs.count
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
@@ -104,12 +125,47 @@ extension AjiteRoomViewController : UICollectionViewDelegateFlowLayout, UICollec
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SongCollectionViewCell", for: indexPath) as! SongCollectionViewCell
-        cell.songName.text = currentAjite.sharedSongs[indexPath.row].name
-        cell.artistName.text = currentAjite.sharedSongs[indexPath.row].artist
-        let data = try? Data(contentsOf: URL(string: currentAjite.sharedSongs[indexPath.row].thumbnailImageUrl)!)
+        cell.songName.text = currentAjite.sharedSongs.songs[indexPath.row].name
+        cell.artistName.text = currentAjite.sharedSongs.songs[indexPath.row].artist
+        let data = try? Data(contentsOf: URL(string: currentAjite.sharedSongs.songs[indexPath.row].thumbnailImageUrl)!)
         DispatchQueue.main.async {
             cell.songImage.image = UIImage(data: data!)
         }
+        cell.cellDelegate = self
+        cell.index = indexPath
         return cell
+    }
+}
+
+extension AjiteRoomViewController : PlaylistSongListProtocol {
+    func toAddToPlaylist(index: Int) {
+        nextSourceIndex = index
+        performSegue(withIdentifier: "addToPlaylistSegue", sender: self)
+    }
+    func toYoutubePlayer(index: Int) {
+        if self.children.count > 0 {
+            if youtubePlayerViewController.index == index {
+
+                let viewControllers:[UIViewController] = self.children
+                for viewContoller in viewControllers{
+                    viewContoller.willMove(toParent: nil)
+                 
+                   viewContoller.view.removeFromSuperview()
+                    viewContoller.removeFromParent()
+                }
+            }
+            else {
+                youtubePlayerViewController.index = index
+                youtubePlayerViewController.playCertainVideo()
+                return
+            }
+        }
+        else {
+            addChildVC()
+            if youtubePlayerViewController.index != index {
+                youtubePlayerViewController.index = index
+                youtubePlayerViewController.playCertainVideo()
+            }
+        }
     }
 }
