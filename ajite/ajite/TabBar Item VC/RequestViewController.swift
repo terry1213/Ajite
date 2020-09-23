d10093560893f554f419fb640e6d06380a4fa503
Yeonwoo Lim <1213terry@naver.com>
2020-09-23 09:50:03 +0900

    친구 신청 수락 혹은 거절 시 테이블 업데이트 구현 및 함수 정리
    친구 신청 수락 혹은 거절 시 테이블 업데이트 구현 및 함수 정리

--- ajite/ajite/TabBar Item VC/RequestViewController.swift
+++ ajite/ajite/TabBar Item VC/RequestViewController.swift
@@ -21,7 +21,7 @@ class RequestViewController: UIViewController{
     var requestUsers : [User] = []
     
     @IBOutlet var RequestTV: UITableView!
-
+    
     
     // ==================================================================>
     
@@ -35,7 +35,7 @@ class RequestViewController: UIViewController{
     
     override func viewWillAppear(_ animated: Bool) {
         //유저의 정보를 가져온다.
-        getUserRequest()
+        getFriendRequest()
     }
     
     // ==================================================================>
@@ -45,51 +45,90 @@ class RequestViewController: UIViewController{
     
     // ======================> Firestore에서 데이터를 가져오거나 저장하는 함수들
     
-    func getUserRequest(){
+    func getFriendRequest(){
         //친구 목록중에 나에게 친구 신청을 보낸(state = 1) 유저의 documentID를 불러온다.
         db
             .collection("users").document(myUser.documentID)
             .collection("friends").whereField("state", isEqualTo: 1).getDocuments{ (snapshot, error) in
-            if let err = error {
-                debugPrint("Error fetching docs: \(err)")
-            }
-            else {
-                guard let snap = snapshot else {return}
-                var count = 0
-                for document in snap.documents {
-                    //유저 documentID를 통해 users document에 접근해서 해당 유저의 모든 정보를 가져온다.
-                    db.collection("users").document(document.documentID).getDocument { (document, error) in
-                        if let document = document, document.exists {
-                            let data = document.data()
-                            //임시로 저장할 유저 변수 생성
-                            let temUser = User()
-                            //유저 이름 저장
-                            temUser.name = data!["name"] as! String
-                            //유저 이메일 아이디 저장
-                            temUser.userID = data!["userID"] as! String
-                            //유저의 프로필 이미지 url 저장
-                            temUser.profileImageURL  = data!["profileImageURL"] as! String
-                            //유저의 documentID 저장
-                            temUser.documentID = document.documentID
-                            //친구 신청 목록에 추가
-                            self.requestUsers.append(temUser)
+                if let err = error {
+                    debugPrint("Error fetching docs: \(err)")
+                }
+                else {
+                    guard let snap = snapshot else {return}
+                    var count = 0
+                    for document in snap.documents {
+                        //유저 documentID를 통해 users document에 접근해서 해당 유저의 모든 정보를 가져온다.
+                        self.getUserData(userDocumentID: document.documentID){
                             count += 1
                             //모든 친구 요청을 불러왔을 경우,
                             if count == snap.documents.count {
                                 //테이블에 불러온 정보를 보여준다.
                                 self.RequestTV.reloadData()
                             }
-                        } else {
-                            print("Document does not exist")
                         }
                     }
                 }
+        }
+    }
+    
+    //유저 documentID를 통해 해당 유저의 정보를 가져오는 함수.
+    func getUserData(userDocumentID : String, completion: @escaping () -> Void){
+        db.collection("users").document(userDocumentID).getDocument { (document, error) in
+            if let document = document, document.exists {
+                let data = document.data()
+                //임시로 저장할 유저 변수 생성
+                let temUser = User()
+                //유저 이름 저장
+                temUser.name = data!["name"] as! String
+                //유저 이메일 아이디 저장
+                temUser.userID = data!["userID"] as! String
+                //유저의 프로필 이미지 url 저장
+                temUser.profileImageURL  = data!["profileImageURL"] as! String
+                //유저의 documentID 저장
+                temUser.documentID = document.documentID
+                //친구 신청 목록에 추가
+                self.requestUsers.append(temUser)
+                completion()
+            } else {
+                print("Document does not exist")
             }
         }
     }
     
+    func changeRequest(userFrom : User, userTo : User, state: Int){
+        db.collection("users").document(userTo.documentID).collection("friends").document(userFrom.documentID).updateData([
+            "userID" : userFrom.userID as Any,
+            "name" : userFrom.name as Any,
+            /*
+             친구 state 설명:
+             0 = 친구 신청 보냄
+             1 = 친구 신청 받음
+             2 = 친구 상태
+             거절하면 document 자체를 삭제
+             */
+            "state" : state
+        ]) { err in
+            if let err = err {
+                print("Error updating document: \(err)")
+            } else {
+                print("Document successfully updated")
+            }
+        }
+    }
+    
+    func deleteRequest(userFrom : User, userTo : User){
+        db
+            .collection("users").document(userTo.documentID)
+            .collection("friends").document(userFrom.documentID).delete() { err in
+                if let err = err {
+                    print("Error deleting document: \(err)")
+                } else {
+                    print("Document successfully deleted")
+                }
+        }
+    }
     // ==================================================================>
-        
+    
 }
 
 extension RequestViewController: UITableViewDataSource, UITableViewDelegate {
@@ -111,7 +150,7 @@ extension RequestViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt
-    indexPath: IndexPath) -> CGFloat {
+        indexPath: IndexPath) -> CGFloat {
         return 65
     }
 }
@@ -119,70 +158,21 @@ extension RequestViewController: UITableViewDataSource, UITableViewDelegate {
 extension RequestViewController: RequestTableViewUser {
     func AcceptClickCell(index: Int) {
         //친구 신청을 보낸 유저의 친구 목록에서 document를 찾아 상태를 친구로 변경한다.(state -> 2)
-        db
-            .collection("users").document(requestUsers[index].documentID)
-            .collection("friends").document(myUser.documentID).updateData([
-                /*
-                 친구 state 설명:
-                    0 = 친구 신청 보냄
-                    1 = 친구 신청 받음
-                    2 = 친구 상태
-                    거절하면 document 자체를 삭제
-                 */
-                "state" : 2
-            ]) { err in
-                if let err = err {
-                    print("Error updating document: \(err)")
-                } else {
-                    print("Document successfully updated")
-                }
-            }
+        self.changeRequest(userFrom: myUser, userTo: requestUsers[index], state: 2)
+        
         //친구 신청을 받은 유저의 친구 목록에서 document를 찾아 상태를 친구로 변경한다.(state -> 2)
-        db
-            .collection("users").document(myUser.documentID)
-            .collection("friends").document(requestUsers[index].documentID).updateData([
-                /*
-                 친구 state 설명:
-                    0 = 친구 신청 보냄
-                    1 = 친구 신청 받음
-                    2 = 친구 상태
-                    거절하면 document 자체를 삭제
-                 */
-                "state" : 2
-            ]) { err in
-                if let err = err {
-                    print("Error updating document: \(err)")
-                } else {
-                    print("Document successfully updated")
-                    self.requestUsers.remove(at: index)
-                    self.RequestTV.reloadData()
-                }
-            }
+        self.changeRequest(userFrom: requestUsers[index], userTo: myUser, state: 2)
+        
+        self.RequestTV.reloadData()
     }
     
     func DeclineClickCell(index: Int) {
         //친구 신청을 보낸 유저의 친구 document를 찾아 삭제한다.
-        db
-            .collection("users").document(requestUsers[index].documentID)
-            .collection("friends").document(myUser.documentID).delete() { err in
-                if let err = err {
-                    print("Error deleting document: \(err)")
-                } else {
-                    print("Document successfully deleted")
-                }
-            }
+        deleteRequest(userFrom: requestUsers[index], userTo: myUser)
+        
         //친구 신청을 받은 유저의 친구 document를 찾아 삭제한다.
-        db
-            .collection("users").document(myUser.documentID)
-            .collection("friends").document(requestUsers[index].documentID).delete() { err in
-                if let err = err {
-                    print("Error deleting document: \(err)")
-                } else {
-                    print("Document successfully deleted")
-                    //친구 요청 목록에서 해당 부분을 삭제하고 테이블 리로드
-                    self.requestUsers.remove(at: index)
-                    self.RequestTV.reloadData()
-                }
-            }
+        deleteRequest(userFrom: myUser, userTo: requestUsers[index])
+        
+        self.RequestTV.reloadData()
     }
 }
