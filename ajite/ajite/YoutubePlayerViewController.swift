3adb8601a70aeef96a625c93ec187e92c5d4a2be
Yeonwoo Lim <1213terry@naver.com>
2020-10-19 23:59:27 +0900

    노래 반복 재생 구현(전체 반복, 한 곡 반복)
    노래 반복 재생 구현(전체 반복, 한 곡 반복)

--- ajite/ajite/YoutubePlayerViewController.swift
+++ ajite/ajite/YoutubePlayerViewController.swift
@@ -14,9 +14,16 @@ class YoutubePlayerViewController: UIViewController, YTPlayerViewDelegate{
     // ======================> 변수, outlet 선언
     
     var index: Int = 0
+    var isLoop: Int = 0
+    var isShuffle: Bool = false
     var youtubeVideos = Playlist()
     
     @IBOutlet weak var youtubePlayer: YTPlayerView!
+    @IBOutlet weak var songName: UILabel!
+    @IBOutlet weak var artist: UILabel!
+    @IBOutlet weak var loopButton: UIButton!
+    @IBOutlet weak var shuffleButton: UIButton!
+    
     // ==================================================================>
     
     // ======================> ViewController와 View 관련 함수들
@@ -31,11 +38,9 @@ class YoutubePlayerViewController: UIViewController, YTPlayerViewDelegate{
     
     func loadFirstSong() {
         self.youtubePlayer.delegate = self;
-        youtubePlayer.load(withVideoId: youtubeVideos.songs[index].videoID, playerVars: [
-            "controls": 1,  //0: 영상 컨트롤 바 사라짐, 1: 컨트롤 바 생성
-            "playsinline": 1,   //0: 전체 화면 재생, 1: playerView 화면 내애서 재생
-        ])
-        //view = youtubePlayer
+        playCertainVideo(index)
+        youtubePlayer.layer.cornerRadius = 15
+        youtubePlayer.layer.masksToBounds = true
     }
     
     //유튜브 플레이어 뷰가 준비 되면 실행될 함수
@@ -48,16 +53,20 @@ class YoutubePlayerViewController: UIViewController, YTPlayerViewDelegate{
     func playerView(_ playerView: YTPlayerView, didChangeTo state: YTPlayerState) {
         //유튜브 플레이어 상태가 '영상 종료'일 경우
         if state == YTPlayerState.ended {
-            //인덱스 1 증가
-            index += 1
-            print("\(index)번째 비디오 종료")
+            print("\(index + 1)번째 비디오 종료")
+            if isLoop == 2 {
+                playCertainVideo(index)
+            }
             //뒤에 노래가 남았으면
-            if index < youtubeVideos.songs.count{
+            else if index < youtubeVideos.songs.count-1{
+                //인덱스 1 증가
+                index += 1
                 //해당 노래를 불러온다.
-                self.youtubePlayer.load(withVideoId: youtubeVideos.songs[index].videoID, playerVars: [
-                    "controls": 1,  //0: 영상 컨트롤 바 사라짐, 1: 컨트롤 바 생성
-                    "playsinline": 1,   //0: 전체 화면 재생, 1: playerView 화면 내애서 재생
-                ])
+                playCertainVideo(index)
+            }
+            else if isLoop == 1 {
+                index = 0
+                playCertainVideo(index)
             }
         }
     }
@@ -65,7 +74,10 @@ class YoutubePlayerViewController: UIViewController, YTPlayerViewDelegate{
     //특정 비디오를 재생하기 위해 사용하는 함수
     func playCertainVideo(_ indexOfSelectedSong: Int) {
         index = indexOfSelectedSong
-        self.youtubePlayer.load(withVideoId: youtubeVideos.songs[indexOfSelectedSong].videoID, playerVars: [
+        songName.text = youtubeVideos.songs[index].name
+        artist.text = youtubeVideos.songs[index].artist
+        
+        self.youtubePlayer.load(withVideoId: youtubeVideos.songs[index].videoID, playerVars: [
             "controls": 1,  //0: 영상 컨트롤 바 사라짐, 1: 컨트롤 바 생성
             "playsinline": 1,   //0: 전체 화면 재생, 1: playerView 화면 내애서 재생
         ])
@@ -73,4 +85,33 @@ class YoutubePlayerViewController: UIViewController, YTPlayerViewDelegate{
     
     // ==================================================================>
     
+    @IBAction func loopButtonTouched(_ sender: Any) {
+        isLoop = (isLoop + 1) % 3
+        switch isLoop {
+        case 0:
+            loopButton.setImage(UIImage(systemName: "repeat"), for: .normal)
+            loopButton.tintColor = .lightGray
+            break
+        case 1:
+            loopButton.tintColor = .systemBlue
+            break
+        case 2:
+            loopButton.setImage(UIImage(systemName: "repeat.1"), for: .normal)
+            break
+        default:
+            break
+        }
+    }
+    
+    @IBAction func shuffleButtonTouched(_ sender: Any) {
+        isShuffle = !isShuffle
+        switch isShuffle {
+        case true:
+            shuffleButton.tintColor = .systemBlue
+            break
+        case false:
+            shuffleButton.tintColor = .lightGray
+            break
+        }
+    }
 }
